9be20fb11c8f11f3a366bad6f85d4754a868664c
Nejc Košmrl <kosmrl.nejc@gmail.com>
2025-05-12 17:20:06 +0200

    repo

---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string povNiz = @"Data Source=C:\Users\kosmr\Desktop\CSharp\Vaje11\filmi.sqlite; Version=3;";
+            SQLiteConnection povezava = new SQLiteConnection(povNiz);
+            povezava.Open();
+            SQLiteCommand ukaz = new SQLiteCommand();
+            ukaz.CommandType = CommandType.Text;
+            ukaz.Connection = povezava;
+
+            Console.WriteLine("Vnesi stavek: ");
+            string preberi = Console.ReadLine();
+
+            ukaz.CommandText = preberi;
+
+
+            SQLiteDataReader rez = ukaz.ExecuteReader();
+            while (rez.Read())
+            {
+                for (int i = 0; i < rez.VisibleFieldCount; i++)
+                {
+                    Console.WriteLine(rez[i].ToString());
+                }
+                Console.WriteLine();
+            }
+            povezava.Close();
+        }
+    }
+}
